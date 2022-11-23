41e18ea8cdc9e33c847a03669b449bbdb7053584
axelersson <90241829+axelersson@users.noreply.github.com>
2022-11-23 13:17:22 +0100

    Hej

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const bookList = [
 
 // const searchInput = document.children[0].children[1].children[1].children[1];
 const searchField = document.getElementById('searchField');
-console.log(searchField)
 searchField.addEventListener("keyup", handleKeyPress);
 /* keydown, keyup */
 
@@ -56,7 +55,34 @@ const filteredList = [];
    renderBookList(filteredList);
 
 }
-function renderBookList(list) {
-    console.log(list);
+function renderBookList(bookList) {
+    let html=`<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`
+      for( let i = 0; i < bookList.length; i++){
+        html += `<li
+                    class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b 
+                    border-indigo-700 cursor-pointer">
+                     ${bookList[i].author} - ${bookList[i].title}
+                </li>`;
+      }      
+    html +=`</ul>`;
+    const existingElement = document.querySelector(".book-list");
+    console.log(existingElement);
+    const root = document.getElementById('root');
+    if(existingElement){
+        root.removeChild(existingElement)
+    }
+   
+    const htmlElement = document.createElement('ul');
+    root.insertAdjacentHTML('beforeend', html);
+
 }
+
+{/* <li
+class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+1
+</li>
+<li
+class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+2
+</li> */}
 
